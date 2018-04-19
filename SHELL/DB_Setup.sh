14c7eded70a17ef0a9c083b0e6e5ba380cc57ba8
root <root@localhost.localdomain>
2018-04-20 01:37:40 +0900

    shell&&SQL add

---
+++ SHELL/DB_Setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+yum -y update
+yum -y install mariadb-server
+systemctl start mariadb
+systemctl enable mariadb
+yum -y install git
+systemctl status mariadb
+
---
+++ SHELL/WebAP_Setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+yum -y update
+yum -y install httpd
+systemctl start httpd
+systemctl enable httpd
+yum install -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/7/x86_64/ius-release-1.0-14.ius.centos7.noarch.rpm
+yum -y update
+yum -y install php70u php70u-pdo php70u-mysqlnd php70u-opcache php70u-xml php70u-mcrypt php70u-gd php70u-devel php70u-mysql php70u-intl php70u-mbstring php70u-bcmath php70u-json php70u-iconv php70u-soap
+systemctl restart httpd.service
+yum -y install git
+systemctl status httpd
